3331c846311d45e67198200f5c54fa6e8a14d8fd
Montori <bohn.malte@web.de>
2019-11-06 19:54:27 +0100

    Updated Calculations
    Updated calculations for costs and serving tables / sinks
    Also improved the input check

--- scripts/CookingCalculator.js
+++ scripts/CookingCalculator.js
@@ -1,73 +1,92 @@
 function calculate()
 {
-  let costVar = 0;
-
+  let costVariety = 0;
   let quantity = 1;
   let variety = 1;
-  let prisoner = parseInt(document.getElementById('inputPrisoner').value);
-  if(isNaN(prisoner)) document.getElementById('inputPrisoner').classList.add('is-invalid');
+
+  let prisoner = 0;
+  let mealsPerDay = 0;
+
+  if(!checkValid()) return;
+
   else document.getElementById('inputPrisoner').classList.remove('is-invalid');
 
-  let mealsday = parseInt(document.getElementById('inputMealsDay').value);
-  if(isNaN(mealsday)) return document.getElementById('inputMealsDay').classList.add('is-invalid');
+  if(isNaN(mealsPerDay)) return document.getElementById('inputMealsDay').classList.add('is-invalid');
   else document.getElementById('inputMealsDay').classList.remove('is-invalid');
 
-  switch (document.getElementById('inputMealQty').value) {
+  switch (document.getElementById('inputMealQty').value)
+  {
     case "Low": quantity = 1; break;
     case "Medium": quantity = 2; break;
     case "High": quantity = 3; break;
     case "Fine Dining" : quantity = 4; break;
   }
 
-  switch (document.getElementById('inputMealVar').value) {
+  switch (document.getElementById('inputMealVar').value)
+  {
     case "None" : variety = 0; break;
     case "Low": variety = 1; break;
     case "Medium": variety = 2; break;
     case "High": variety = 3; break;
   }
 
-  switch (document.getElementById('inputMealVar').value) {
-    case "None": costVar = 0; break;
-    case "Low": costVar = quantity*2; break;
-    case "Medium": costVar = quantity*6; break;
-    case "High": costVar = quantity*10; break;
+  switch (document.getElementById('inputMealVar').value)
+  {
+    case "None": costVariety = 0; break;
+    case "Low": costVariety = quantity*2-quantity; break;
+    case "Medium": costVariety = quantity*6-quantity; break;
+    case "High": costVariety = quantity*10-quantity; break;
   }
 
-  let ingreds = Math.floor(variety * prisoner * 1.5);
-  let ingredsCooked = quantity*prisoner;
-  let cookers = Math.floor(quantity == 1 ? prisoner/30 : quantity == 2 ? prisoner/20 : quantity == 3 ? prisoner/10 : prisoner/5);
-  let fridges = Math.floor(variety == 1 ? cookers*1.3 : variety == 2 ? cookers*1.7 : variety == 3 ? cookers*2 : cookers);
+  let cookers = Math.ceil(quantity == 1 ? prisoner/30 : quantity == 2 ? prisoner/20 : quantity == 3 ? prisoner/10 : prisoner/5);
+  let fridges = Math.ceil(variety == 1 ? cookers*1.3 : variety == 2 ? cookers*1.7 : variety == 3 ? cookers*2 : cookers);
   let cooks = cookers+2;
-  let bins = 1;
-  let sinks = Math.floor(prisoner/30);
-  let benches = Math.floor(prisoner/4);
-  let tables = Math.floor(benches/2);
-  let servingTables = Math.floor(ingredsCooked/80);
-  let costMeal = quantity+costVar;
-  let costDaily = costMeal*prisoner*mealsday;
-
-  if(sinks < 1) sinks = 1;
-  if(fridges < 1) fridges = 1;
-  if(cookers < 1) cookers = 1;
-  if(cooks < 1) cooks = 1;
-  if(benches < 1) benches = 1;
-  if(tables < 1) tables = 1;
-  if(servingTables < 1) servingTables = 1;
+  let benches = Math.ceil(prisoner/4);
+  let tables = Math.ceil(prisoner/8);
+  let servingTables = Math.ceil(prisoner/40);
+  let sinks = Math.ceil(servingTables+1);
+  let costSingleMeal = quantity+costVariety;
+  let costDailyTotal = costSingleMeal*prisoner*mealsPerDay;
 
   document.getElementById('resultKitchenCooks').innerHTML = cooks;
   document.getElementById('resultKitchenCookers').innerHTML = cookers;
   document.getElementById('resultKitchenFridges').innerHTML = fridges;
   document.getElementById('resultKitchenSinks').innerHTML = sinks;
-  document.getElementById('resultKitchenBins').innerHTML = bins;
+  document.getElementById('resultKitchenBins').innerHTML = "1";
 
   document.getElementById('resultCanteenTables').innerHTML = tables;
   document.getElementById('resultCanteenBenches').innerHTML = benches;
   document.getElementById('resultCanteenServing').innerHTML = servingTables;
-  document.getElementById('resultCanteenBins').innerHTML = bins;
+  document.getElementById('resultCanteenBins').innerHTML = "1";
 
   document.getElementById('resultCostQty').innerHTML = `$${quantity}.00`;
-  document.getElementById('resultCostVar').innerHTML = `$${costVar}.00`;
-  document.getElementById('resultCostMeal').innerHTML = `$${costMeal}.00`;
-  document.getElementById('resultCostDaily').innerHTML = `$${costDaily}.00`;
+  document.getElementById('resultCostVar').innerHTML = `$${costVariety}.00`;
+  document.getElementById('resultCostMeal').innerHTML = `$${costSingleMeal}.00`;
+  document.getElementById('resultCostDaily').innerHTML = `$${costDailyTotal}.00`;
 
+  function checkValid()
+  {
+    let resultP = false;
+    let resultM = false;
+
+    let prisonerInput = document.getElementById("inputPrisoner");
+    let mealsInput = document.getElementById("inputMealsDay");
+
+    if(prisonerInput.value.trim() === "" || isNaN(parseInt(prisonerInput.value)) || parseInt(prisonerInput.value) <= 0) prisonerInput.classList.add("is-invalid");
+    else
+    {
+      resultP = true;
+      prisoner = parseInt(prisonerInput.value);
+      prisonerInput.classList.remove("is-invalid");
+    }
+
+    if(mealsInput.value.trim() === "" || isNaN(parseInt(mealsInput.value)) || parseInt(mealsInput.value) <= 0) mealsInput.classList.add("is-invalid");
+    else
+    {
+      resultM = true;
+      mealsPerDay = parseInt(mealsInput.value);
+      mealsInput.classList.remove("is-invalid");
+    }
+    return resultP && resultM;
+  }
 }
