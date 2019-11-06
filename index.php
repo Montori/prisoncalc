372416ea0b639bbc546da34cfb220c794c23358c
Montori <bohn.malte@web.de>
2019-11-06 19:53:47 +0100

    Updated welcome message

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   <div class="card bg-dark text-white text-center">
     <div class="card-body">
       <h1 style="font-size:24px;">Prison Architect Calculator</h1>
-      <p>Heyho!</p>
+      <p>Greetings!</p>
       <p>As you could probably guess this is a website to calculate your kitchens, canteens, laundry etc.</p>
       <br>
       The website itself is made by me <a class="text-white-50" href="https://github.com/Montori">(Montori)</a> and the calculations were done by <a class="text-white-50" href="reddit.com/u/giantb">GiantB</a> based on his google sheet
       <br>
       If you want to check out the original google sheet you can click <a class="text-white-50" href="https://docs.google.com/spreadsheets/d/1NXZT3-Ebb9meEeVSpcQbvMTfpMV0qCN6KtHTE9ho1-s/edit#gid=646532267">here</a>!
+      <br><br>
+      Feel free to report bugs or request new features via github
     </div>
   </div>
 </div>
