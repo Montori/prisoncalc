d7a7eeb4343b5ce21fc94a7bdac3989b6b775f5c
Montori <bohn.malte@web.de>
2019-11-09 17:24:51 +0100

    altered index for the power calculations

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
       <p>Greetings!</p>
       <p>As you could probably guess this is a website to calculate your kitchens, canteens, laundry etc.</p>
       <br>
-      The website itself is made by me <a class="text-white-50" href="https://github.com/Montori">(Montori)</a> and the calculations were done by <a class="text-white-50" href="reddit.com/u/giantb">GiantB</a> based on his google sheet
+      The website itself is made by me <a class="text-white-50" href="https://github.com/Montori">(Montori)</a>, the calculations were done by <a class="text-white-50" href="reddit.com/u/giantb">GiantB</a> based on his google sheet and the power calculations were done by JaffaCakes6 based on his <a class="text-white-50" href="https://farm8.staticflickr.com/7355/10899701806_1979cbcd64_o.png">power guide</a>
       <br>
       If you want to check out the original google sheet you can click <a class="text-white-50" href="https://docs.google.com/spreadsheets/d/1NXZT3-Ebb9meEeVSpcQbvMTfpMV0qCN6KtHTE9ho1-s/edit#gid=646532267">here</a>!
-      <br><br>
+      <br>
+      <br>
       Feel free to report bugs or request new features via github
     </div>
   </div>
